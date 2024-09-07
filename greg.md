6114e1c1cde9f2a96f4f9a5318363f57f75d90a7
Omena0 <Omena0MC@gmail.com>
2024-09-07 22:03:56 +0300

    greg

--- greg.md
+++ greg.md
@@ -1,14 +1,4 @@
 
 # Greg generator
 
-```py
-import random
-
-def greg():
-    """Greg generator"""
-    names = ["Greg"]
-    verbs = ["wrote", "developed", "created", "made", "invented", "did"]
-    adjectives = ["amazing", "cool", "awesome", "fantastic", "great", "incredible", "outstanding", "marvelous", "superb", "brilliant"]
-    things = ["a game", "a website", "a script", "a program", "a software", "an app", "an AI", "a robot", "a machine learning model", "a neural network"]
-    return "{} {} {} {}".format(random.choice(names), random.choice(verbs), random.choice(adjectives), random.choice(things))
-```
+greg
--- greg.py
+++ greg.py
@@ -1,25 +1,54 @@
-import random
+# sourcery skip: for-append-to-extend
+import random as r
 
-names = ["Greg"]
-verbs = ["wrote", "developed", "created", "made", "invented", "did", "generated", "compiled", "implemented", "debugged", "programmed", "coded", "greg", "gregged", "did"]
-adjectives = ["an amazing", "a cool", "an awesome", "a fantastic", "a great", "an incredible", "an outstanding", "a marvelous", "a superb", "a brilliant", "an insane", "a fire", "greg", "a W", "a cool", "a real"]
-things = ["game", "website", "script", "program", "software", "app", "AI", "robot", "machine learning model", "neural network", "greg", "greg generator", "programming language", "compiler", "interperter", "chat application"]
+names = ['Yousuuf gaimar', "Greg", "Gregory", "Useef gaimar"]
 
+titleVerbs = ['Slayer', 'Destroyer', 'Obliterator']
+titles_ = ["The .. of gods", "The .. of gregs", "The chosen one", "The greg", "The gregger"]
+suffixes = ["the I", "the II", "the III", "the IV", "of the Greg", "of the Gregory"]
 
-def greg():
-    """Greg generator"""
+verbs = ["wrote", "developed", "created", "made", "invented", "did", "generated", "compiled", "implemented", "debugged", "programmed", "coded", "greg", "gregged", "did", "coped on", "is", "is not"]
+adjectives = ["an amazing", "a cool", "an awesome", "a fantastic", "a great", "an incredible", "an outstanding", "a marvelous", "a superb", "a brilliant", "an insane", "a fire", "a greg", "a W", "a cool", "a real", "a fucking"]
+things = ["game", "website", "script", "program", "software", "app", "AI", "robot", "machine learning model", "neural network", "greg", "greg generator", "programming language", "compiler", "interperter", "chat application", "thing"]
+
+shuffle = False
+
+titles = []
+for title in titles_:
+    if '..' not in title:
+        titles.append(title)
+        continue
+
+    for verb in titleVerbs:
+        titles.append(title.replace('..', verb))
+
+def greg_names_frfr_ong():  # sourcery skip: for-append-to-extend
+    """greg greg generator"""
     gregs = []
     for name in names:
+        for title in titles:
+            for suffix in suffixes:
+                gregs.append(f'{name} {title} {suffix}')
+
+    if shuffle: r.shuffle(gregs)
+    return gregs
+
+
+def greg():  # sourcery skip: for-append-to-extend
+    """Greg generator"""
+    gregs_did_what = []
+    gregs = greg_names_frfr_ong()
+    for greg in gregs:
         for verb in verbs:
             for adjective in adjectives:
                 for thing in things:
-                    gregs.append(f'{name} {verb} {adjective} {thing}\n')
-    return gregs
+                    gregs_did_what.append(f'{greg} {verb} {adjective} {thing}\n')
 
-i = 0
+    if shuffle: r.shuffle(gregs_did_what)
+    return gregs_did_what
 
-try:
-    gregs = greg()
-except KeyboardInterrupt: ...
+gregs = greg()
+
+if shuffle: r.shuffle(gregs)
 
 with open('greg.txt','w') as f: f.writelines(gregs)
