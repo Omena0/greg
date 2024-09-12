f5d15f0a2ab935041d283bb61b1d582ddd31fb25
Omena0 <Omena0MC@gmail.com>
2024-09-12 20:23:31 +0300

    gregalg

--- gregalg.py
+++ gregalg.py
@@ -1,25 +1,47 @@
-import re
+import time as t
+from multiprocessing import Pool
+import numpy as np
 
-# Initialize variables to keep track of the line with the most "greg"s
-max_greg_line = ""
-max_greg_count = 0
+def func(lines):
+    max_gregs = ''
+    count = 0
+    for i in lines:
+        linecount = i.count('greg')
+        if linecount > count:
+            count = linecount
+            max_gregs = i
 
-i = 0
+    return max_gregs
 
-# Open the file in read mode
-with open('greg.txt', 'r') as file:
-    # Loop through each line in the file
-    for line in file:
-        print(f'Line: {i}')
-        # Count the number of "greg"s in the line using regex
-        greg_count = len(re.findall(r'\bgreg\b', line))
-        
-        # If this line has more "greg"s than the previous max, update the max
-        if greg_count > max_greg_count:
-            max_greg_line = line.strip()
-            max_greg_count = greg_count
+if __name__ == '__main__':
 
-        i += 1
+    start = t.time()
 
-# Print the line with the most "greg"s
-print(max_greg_line)
+    # Open the file in greg mode
+    with open('greg.txt') as file:
+        lines = file.readlines()
+
+    print(f'File read in {t.time()-start} Seconds')
+
+    start = t.time()
+    lines_2_0 = np.array_split(lines, 4)
+
+    print(f'Array split in {t.time()-start} Seconds')
+
+    start = t.time()
+    with Pool(4) as p:
+        result = p.map(func,lines_2_0)
+
+    print(f'Computed gregs in {t.time()-start} Seconds')
+
+
+    max_gregs = ''
+    count = 0
+    for i in result:
+        linecount = i.count('greg')
+        if linecount > count:
+            count = linecount
+            max_gregs = i
+
+    print(max_gregs)
+
